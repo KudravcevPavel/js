f82c7782280d45fd68b32d9e59da65cb5a47139e
KudravcevPavel <kudravcev92@gmail.com>
2020-09-15 21:51:53 +0300

    person

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 /* eslint-disable no-console */
 
+const createPerson = (fn, ln, mn, bd) => ({
+  firstName: fn,
+  lastName: ln,
+  middleName: mn,
+  birthDate: bd,
+});
+
+const generatePeople = () => {
+  return [
+    createPerson("Pavel", "Kudravcev", "Alexeevich", "20.10.92"),
+    createPerson("Ivan", "Ivanov", "Edouardovich", "01.03.71"),
+    createPerson("Ilia", "Smirnov", "Glebovich", "05.11.87"),
+    createPerson("Alex", "Kuznecov", "Igorevich", "07.12.99"),
+    createPerson("Den", "Popov", "Nikitich", "09.01.95"),
+    createPerson("Vlad", "Petrov", "Rustamovich", "25.06.90"),
+    createPerson("Andrei", "Sokolov", "Savelyevich", "05.05.95"),
+    createPerson("Andrei", "Sokolov", "Vladislavovich", "07.08.99"),
+    createPerson("Anton", "Chernov", "Stanislavovich", "15.03.93"),
+    createPerson("Ben", "Pavlov", "Olegovich", "18.09.90"),
+  ];
+};
+
+const comparePerson = (a, b) => {
+  if (a.firstName === b.firstName) {
+    if (a.lastName === b.lastName) {
+      return a.middleName > b.middleName ? -1 : 1;
+    }
+    return a.lastName > b.lastName ? -1 : 1;
+  }
+  return a.firstName > b.firstName ? -1 : 1;
+};
+
+const sortAnything = (arr, compare, direction = 1) => {
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = 0; j < arr.length - 1 - i; j += 1) {
+      if (compare(arr[j], arr[j + 1]) === direction) {
+        const t = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = t;
+      }
+    }
+  }
+};
+
+const people = generatePeople();
+
+sortAnything(people, comparePerson, -1);
+
+console.log(people);
+
 /* 
 // задание №1
 const getDistance = (x1, y1, x2, y2) =>
@@ -22,7 +72,7 @@ showResult(px1, py1, px2, py2, d); */
 
 // задание №1 (oject)
 
-const getDistance = (x1, y1, x2, y2) =>
+/* const getDistance = (x1, y1, x2, y2) =>
   Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
 const showResult = (point, d) =>
@@ -38,7 +88,7 @@ const point = {
 };
 const d = getDistance(point.x1, point.y1, point.x2, point.y2);
 
-showResult(point, d);
+showResult(point, d); */
 
 // задание №2
 
@@ -50,11 +100,9 @@ const a = Triangle(ax, bx, cx);
 console.log(`Теорема Пифагора для чисел ${ax}, ${bx}, ${cx}, ${a}`);
  */
 
-
-
 // задание №2 (Object)
 
-const Triangle = (a, b, c) => a ** 2 + b ** 2 === c ** 2;
+/* const Triangle = (a, b, c) => a ** 2 + b ** 2 === c ** 2;
 const lengths = {
   a: 10,
   b: 15,
@@ -62,12 +110,11 @@ const lengths = {
 }
 const a = Triangle(lengths.a, lengths.b, lengths.c);
 console.log(`Теорема Пифагора для чисел ${lengths.a}, ${lengths.b}, ${lengths.c}, ${a}`);
-
+ */
 
 // задание №3
 
-
-function triangle(a, b, c) {
+/* function triangle(a, b, c) {
   const check1 = a + b > c;
   const check2 = b + c > a;
   const check3 = c + a > b;
@@ -77,11 +124,11 @@ function triangle(a, b, c) {
   ${a}, ${b}, ${c} ${action} построить треугольник`);
 }
 
-triangle(5, 18, 8);
+triangle(5, 18, 8); */
 
 // задание №4  (аполнить массив 20 числами которые делятся на 3, или на 5, или на 15 и вывести его на экран )
 
-const numbers = [];
+/* const numbers = [];
 
 let number = 3;
 
@@ -96,11 +143,11 @@ function numb(length, a, b, c) {
   }
   return numbers;
 }
-console.log(numb(20, 3, 5, 15));
+console.log(numb(20, 3, 5, 15)); */
 
 // задание №5 (посчитать произведение всех элементов находящихся в каждой третьей позиции)
 
-const thirdposition = () => {
+/* const thirdposition = () => {
   let position = 1;
 
   for (let i = 0; i < numbers.length; i += 3) {
@@ -111,11 +158,11 @@ const thirdposition = () => {
   );
 };
 
-thirdposition();
+thirdposition(); */
 
 // задание №6 (посчитать среднее арифметическое всех чисел)
 
-const Sumall = () => {
+/* const Sumall = () => {
   let sum = 0;
 
   for (let i = 0; i < numbers.length; i += 1) {
@@ -126,10 +173,10 @@ const Sumall = () => {
 };
 
 Sumall();
-
+ */
 // задание №7 (найти максимальное и минимальное число)
 
-const Max = () => {
+/* const Max = () => {
   let max = numbers[0];
   let min = numbers[0];
   for (let i = 1; i < numbers.length; i += 1) {
@@ -141,11 +188,11 @@ const Max = () => {
 
   return console.log(`максимальное число  ${max}, ${min}`);
 };
-Max();
+Max(); */
 
 // задание №8 (вывести матрицу таблицы умножения)
 
-const matrix = [];
+/* const matrix = [];
 
 
 
@@ -170,4 +217,4 @@ function tab(a, b) {
 }
 
 tab(10, 5);
-
+ */
