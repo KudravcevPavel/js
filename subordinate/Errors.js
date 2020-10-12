179bd6b34f2eea199747a55988986ac4ccc4e14a
KudravcevPavel <kudravcev92@gmail.com>
2020-10-12 22:00:02 +0300

    validator

---
+++ subordinate/Errors.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+export function dumpErrors(errorList, validatedObject) {
+   errorList.forEach((error) => {
+      console.log(`Field: ${error.key}`);
+      console.log(`Provided value: ${validatedObject[error.key]}`);
+      console.log(`Message: ${error.message}`);
+   });
+}
---
+++ subordinate/information.js
@@ -0,0 +1,58 @@
+
+
+import {checkType, nonEmptyString, numberInRange} from "./validators.js";
+
+export const user = {
+   firstName: "test",
+   lastName: "test",
+   age: 78,
+   phone: "212412342134",
+};
+
+export const userScheme = {
+   firstName: [{
+         /* validator: {
+           params: ["string"],
+           rule: checkType
+         } */
+         validationRule: checkType,
+         validationParams: ["string"],
+         validationMessage: "First Name not string",
+      },
+      {
+         validationRule: nonEmptyString,
+         validationParams: [],
+         validationMessage: "First Name is empty",
+      },
+   ],
+   lastName: [{
+         validationRule: checkType,
+         validationParams: ["string"],
+         validationMessage: "Last Name not string",
+      },
+      {
+         validationRule: nonEmptyString,
+         validationParams: [],
+         validationMessage: "Last Name is empty",
+      },
+   ],
+   age: [{
+         validationRule: checkType,
+         validationParams: ["number"],
+         validationMessage: "Age not number",
+      },
+      {
+         validationRule: numberInRange,
+         validationParams: [10, 56],
+         validationMessage: `Age not in range between 10 and 56`,
+      },
+   ],
+   phone: [{
+      validationRule: checkType,
+      validationParams: ["string"],
+      validationMessage: "Phone not string",
+   }, ],
+};
+
+user.firstName = "";
+user.age = 40;
